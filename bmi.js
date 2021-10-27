7273223f6914b7a8b0ee96d17baeaec5ec7686ef
Samita <samitaawale389@gmail.com>
2021-10-27 11:11:32 +0545

    complete

---
+++ bmi.js
@@ -0,0 +1,15 @@
+//BMI = KG /( H/100 * H/100)
+
+document.getElementById('submit').addEventListener('click', bmiCalculator);
+function bmiCalculator(){
+    var cm= parseInt(document.getElementById('cm').value);
+    var kg= parseInt(document.getElementById('kg').value);
+     var bmi;
+     var newCm= parseFloat(cm/100);
+     bmi= kg /(newCm* newCm);
+     bmi= bmi.toFixed(1);
+     console.log(bmi);
+     document.getElementById('result').innerHTML = bmi;
+
+
+}
